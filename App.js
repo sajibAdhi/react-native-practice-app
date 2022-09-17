e013fc484564eb97b2b5a5cb8498218f46b3b6eb
sajibAdhi <sajibadhikery@gmail.com>
2022-09-17 20:22:45 +0600

    15. Styling React Native Apps

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another Piece of text!</Text>
+        <Text style={styles.dummyText}>Another Piece of text!</Text>
       </View>
-      <Text>Hello World!</Text>
-        <Button title='Tap Me'/>
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Button title='Tap Me' />
     </View>
   );
 }
@@ -20,4 +20,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "#ff00ff",
+  }
 });
