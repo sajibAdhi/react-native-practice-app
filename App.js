39e64e95bd7d3cc3390834346621f56dab9a68ae
Sajib Adhikary <60180521+sajibAdhi@users.noreply.github.com>
2022-09-24 09:11:19 +0000

    style height weidth

--- App.js
+++ App.js
@@ -8,6 +8,10 @@ export default function App() {
         <Text style={styles.dummyText}>Another Piece of text!</Text>
       </View>
       <Text style={styles.dummyText}>Hello World!</Text>
+      <Text style={styles.red}>just red</Text>
+      <Text style={[styles.bigBlue, styles.flex1]}>just bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
       <Button title='Tap Me' />
     </View>
   );
@@ -19,6 +23,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  flex1: {
+    flex: 1,
+  },
+  flex2: {
+    flex: 2,
+  },
+  flex3: {
+    flex: 3,
   },
   dummyText: {
     margin: 16,
